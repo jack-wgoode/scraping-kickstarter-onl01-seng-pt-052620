318e70f09a7bb1a88baf393813ca053c6c350bc1
Jack Goode <jack.w.goode@icloud.com>
2020-07-02 13:13:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,6 +16,7 @@ def create_project_hash
   #iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project| 
     projects[project] = {} 
+    binding.pry
   end
   #return the projects hash 
   projects

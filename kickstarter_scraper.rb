212b7f8d3f364218e7907d095e02523219d6c3f2
Jack Goode <jack.w.goode@icloud.com>
2020-07-02 13:28:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -21,9 +21,7 @@ def create_project_hash
       :description => project.css("p.bbcard_blurb").text
       :location => project.css("ul.project-meta span.location-name").text
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-
-    } 
-    #exitbinding.pry
+    }
   end
   #return the projects hash 
   projects
